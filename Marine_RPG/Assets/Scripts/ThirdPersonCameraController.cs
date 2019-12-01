6c9d3fdd6bf18bb7c186ab316832e9520d6e6b70
YoonJuHwan <yoon987654@khu.ac.kr>
2019-12-02 00:28:31 +0900

    유니티 프로젝트 생성 && 프로토타입 에셋 세팅

---
+++ Marine_RPG/Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    [Header("Camera Settings")]
+    public float smoothTime = 0.5f;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    private Camera myCam;
+
+    private void Awake()
+    {
+        myCam = GetComponent<Camera>();
+        offset = transform.position - PlayerController.instance.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        UpdateCamPos();
+    }
+
+    private void UpdateCamPos()
+    {
+        Vector3 currentVelocity = Vector3.zero;
+        transform.position = Vector3.SmoothDamp(transform.position, PlayerController.instance.transform.position + offset, ref currentVelocity, smoothTime);
+    }
+}
